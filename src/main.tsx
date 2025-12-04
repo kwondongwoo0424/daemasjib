3ffda72ff04e6d8774416c7cbccb52a1546ea9b3
kwondongwoo0424 <hyeonggyugwon3@gmail.com>
2025-12-04 10:02:13 +0900

    pwa

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { registerSW } from "virtual:pwa-register";
 
-createRoot(document.getElementById('root')!).render(
+registerSW({
+  onNeedRefresh() {
+    console.log("새 버전이 있습니다.");
+  },
+  onOfflineReady() {
+    console.log("오프라인 사용 가능합니다.");
+  },
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Daemasjib",
+        short_name: "Daemasjib",
+        start_url: "/daemasjib/",
+        scope: "/daemasjib/",
+        id: "/daemasjib/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
   server: {
     proxy: {
-      '/api/daegu': {
-        target: 'https://www.daegufood.go.kr',
+      "/api/daegu": {
+        target: "https://www.daegufood.go.kr",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/daegu/, '/kor/api/tasty.html'),
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api\/daegu/, "/kor/api/tasty.html"),
+      },
+    },
+  },
+});
